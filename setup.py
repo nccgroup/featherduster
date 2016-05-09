0095328a22fe155226373268327c09fe4ce9ce95
Daniel Crowley <dcrowley@isecpartners.com>
2016-05-09 14:17:00 -0500

    Added setuptools installer to simplify install process

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='featherduster',
+      version='0.1',
+      description='An automated cryptanalysis tool',
+      url='http://github.com/nccgroup/featherduster',
+      author='Daniel "unicornfurnace" Crowley',
+      author_email='daniel.crowley@nccgroup.trust',
+      license='BSD',
+      packages=['cryptanalib','feathermodules'],
+      install_requires=[
+          'gmpy',
+          'pycrypto'
+      ],
+      zip_safe=False)
