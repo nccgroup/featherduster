650b6f87e53c231f2f8cebbac3873ce22c507c91
Daniel Crowley <dcrowley@isecpartners.com>
2016-06-06 17:28:23 -0500

    Bugfix for hash format detector

--- feathermodules/auxiliary/hash_format_detect.py
+++ feathermodules/auxiliary/hash_format_detect.py
@@ -4,8 +4,8 @@ import feathermodules
 def detect_hash_format(hashes):
    options = feathermodules.current_options
    if options['words'] == '' or ',' not in options['words']:
-      words = raw_input('Please enter words that might be in the plaintext version of the hashes, comma-separated: ').split(',')
-   return ca.detect_hash_format(words,hashes)
+      options['words'] = raw_input('Please enter words that might be in the plaintext version of the hashes, comma-separated: ').split(',')
+   return ca.detect_hash_format(options['words'],hashes)
 
 
 feathermodules.module_list['detect_hash_format'] = {
@@ -13,5 +13,5 @@ feathermodules.module_list['detect_hash_format'] = {
    'type':'auxiliary',
    'keywords':['md_hashes','sha1_hashes','sha2_hashes'],
    'description':'Try hashing all permutations of provided words with different delimiters to determine how data is put together before hashing.',
-   'options':{'words':''}
+   'options':{'words':'firstword,secondword'}
 }
