f8672decb4ef56349f4b8082a4357af6762c75c3
dcrowley <dcrowley@unicornFurnace.local>
2016-10-04 14:58:45 -0500

    Removed need for user input in ciphertext analysis test

--- tests/test_ciphertext_analysis.py
+++ tests/test_ciphertext_analysis.py
@@ -1,7 +1,9 @@
 from Crypto import Random
 from Crypto.Cipher import AES
 import cryptanalib as ca
-from zlib import compress
+from zlib import compress, decompress
+
+print 'Testing ciphertext analysis engine...'
 
 plaintext = 'I am the very model of a modern major-general, I\'ve information vegetable, animal and mineral, I know the kings of England and I quote the fights historical, from Marathon to Waterloo in order categoricalAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
 
@@ -39,57 +41,67 @@ ecb_cipher = AES.new(key, AES.MODE_ECB, iv)
 ecb_ciphertexts = [ecb_cipher.encrypt(ca.pkcs7_pad(plaintext, AES.block_size))]
 ecb_cipher = AES.new(key, AES.MODE_ECB, iv)
 ecb_ciphertexts.append(ecb_cipher.encrypt(ca.pkcs7_pad(plaintext2,AES.block_size)))
-print 'ECB ciphertexts are:'
-print "\n".join([ct.encode('hex') for ct in ecb_ciphertexts])
 
 cbc_cipher = AES.new(key, AES.MODE_CBC, iv)
 cbc_ciphertexts = [cbc_cipher.encrypt(ca.pkcs7_pad(plaintext,AES.block_size))]
 cbc_cipher = AES.new(key, AES.MODE_CBC, iv)
 cbc_ciphertexts.append(cbc_cipher.encrypt(ca.pkcs7_pad(plaintext2,AES.block_size)))
-print 'CBC fixed IV ciphertexts are:'
-print "\n".join([ct.encode('hex') for ct in cbc_ciphertexts])
 
 mb_xor_ciphertexts = [ca.sxor(plaintext,"\xfa\x4e\x77\x01\x43"*len(plaintext))]
 mb_xor_ciphertexts.append(ca.sxor(plaintext2,"\xfa\x4e\x77\x01\x43"*len(plaintext2)))
-print 'Multi-byte XOR ciphertexts are:'
-print "\n".join([ct.encode('hex') for ct in mb_xor_ciphertexts])
 
 two_time_pad_ciphertexts = [ca.sxor(plaintext,two_time_pad_key)]
 two_time_pad_ciphertexts.append(ca.sxor(plaintext2,two_time_pad_key))
-print 'Two-time pad ciphertexts are:'
-print "\n".join([ct.encode('hex') for ct in two_time_pad_ciphertexts])
 
 compressed_messages = [compress(plaintext), compress(plaintext2)]
 
 print 'Analyzing ECB ciphertexts...'
-ca.analyze_ciphertext(ecb_ciphertexts,verbose=True)
-print ''
-print 'Analyzing CBC fixed-IV ciphertexts...'
-ca.analyze_ciphertext(cbc_ciphertexts,verbose=True)
-print ''
-print 'Analyzing multi-byte XOR ciphertexts...'
-ca.analyze_ciphertext(mb_xor_ciphertexts,verbose=True)
-print ''
-print 'Analyzing two-time pad ciphertexts...'
-ca.analyze_ciphertext(two_time_pad_ciphertexts,verbose=True)
-print ''
-print 'Analyzing hex-encoded ciphertext...'
-ca.analyze_ciphertext([ct.encode('hex') for ct in ecb_ciphertexts],verbose=True)
-print ''
-print 'Analyzing base64-encoded ciphertext...'
-ca.analyze_ciphertext([ct.encode('base64') for ct in ecb_ciphertexts],verbose=True)
-print ''
-print 'Analyzing compressed plaintexts...'
-ca.analyze_ciphertext(compressed_messages,verbose=True)
-print ''
-print 'Analyzing critically small RSA key...'
-ca.analyze_ciphertext([RSA_pubkey_critically_small_n],verbose=True)
-print ''
-print 'Analyzing very small RSA key...'
-ca.analyze_ciphertext([RSA_pubkey_very_small_n],verbose=True)
-print ''
-print 'Analyzing small RSA key...'
-ca.analyze_ciphertext([RSA_pubkey_small_n],verbose=True)
+ecb_results = ca.analyze_ciphertext(ecb_ciphertexts)
+if 'ecb' not in ecb_results['keywords']:
+   exit('ECB detection is broken.')
 
-if raw_input('Were the ciphertexts properly identified (yes)?').lower() not in ['y','yes','']:
-   raise Exception('Ciphertext analysis is broken.')
+print 'Analyzing CBC fixed-IV ciphertexts...'
+cbc_results = ca.analyze_ciphertext(cbc_ciphertexts)
+if 'cbc_fixed_iv' not in cbc_results['keywords']:
+   exit('CBC fixed IV detection is broken.')
+
+print 'Analyzing multi-byte XOR ciphertexts...'
+multi_byte_xor_results = ca.analyze_ciphertext(mb_xor_ciphertexts)
+if 'individually_low_entropy' not in multi_byte_xor_results['keywords']:
+   exit('Multi-byte XOR not flagged as weak crypto.')
+
+print 'Analyzing two-time pad ciphertexts...'
+two_time_pad_results = ca.analyze_ciphertext(two_time_pad_ciphertexts)
+if 'key_reuse' not in two_time_pad_results['keywords']:
+   exit('Key reuse detection is broken.')
+
+print 'Analyzing hex-encoded ciphertext...'
+hex_results = ca.analyze_ciphertext([ct.encode('hex') for ct in ecb_ciphertexts])
+if hex_results['decoded_ciphertexts'][0] != ecb_ciphertexts[0]:
+   exit('Hex encoding detection is broken.')
+
+print 'Analyzing base64-encoded ciphertext...'
+base64_results = ca.analyze_ciphertext([ct.encode('base64') for ct in ecb_ciphertexts])
+if base64_results['decoded_ciphertexts'][0] != ecb_ciphertexts[0]:
+   exit('Base64 encoding detection is broken.')
+
+print 'Analyzing compressed plaintexts...'
+compressed_results = ca.analyze_ciphertext(compressed_messages)
+if compressed_results['decoded_ciphertexts'][0] != decompress(compressed_messages[0]):
+   exit('Zlib compression detection is broken.')
+
+print 'Analyzing critically small RSA key...'
+crit_rsa_length_results = ca.analyze_ciphertext([RSA_pubkey_critically_small_n])
+if 'rsa_small_n' not in crit_rsa_length_results['keywords']:
+   exit('Small RSA modulus detection is broken.')
+
+print 'Analyzing very small RSA key...'
+very_small_rsa_length_results = ca.analyze_ciphertext([RSA_pubkey_very_small_n])
+if 'rsa_small_n' not in very_small_rsa_length_results['keywords']:
+   exit('Small RSA modulus detection is broken.')
+
+print 'Analyzing small RSA key...'
+small_rsa_length_results = ca.analyze_ciphertext([RSA_pubkey_small_n])
+if 'rsa_small_n' not in small_rsa_length_results['keywords']:
+   exit('Small RSA modulus detection is broken.')
+
